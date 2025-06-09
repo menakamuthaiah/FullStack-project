1670c397dc769e3eee07cac65662c38c9376df0f
menakamuthaiah <muthaiahgokul8810@gmail.com>
2025-06-09 14:13:55 +0530

    Added files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
